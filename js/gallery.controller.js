53463d1e6c3a5e3c75393af6b0f4c51edda85ef3
DvirFramework <78683107+DvirFramework@users.noreply.github.com>
2023-11-30 21:35:11 +0200

    subttion-Thursday-2000

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -7,7 +7,6 @@ var gGalleryImgs = [
 
 function renderGallery() {
     const gallerySection = document.querySelector('.image-gallery')
-
     gallerySection.innerHTML = ''
 
     gGalleryImgs.forEach(img => {
@@ -30,6 +29,7 @@ function onImgSelect(selectedImgId) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const galleryImages = document.querySelectorAll('.image-gallery img')
+
     galleryImages.forEach(imgElement => {
         imgElement.addEventListener('click', function () {
             const selectedImgId = parseInt(imgElement.getAttribute('data-img-id'))
@@ -37,3 +37,17 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     })
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryLink = document.getElementById('gallery-link')
+    const imageGallery = document.querySelector('.image-gallery')
+
+    galleryLink.addEventListener('click', function () {
+        imageGallery.classList.toggle('hidden')
+    })
+})
+
+function hideGallery() {
+    const imageGallery = document.querySelector('.image-gallery');
+    imageGallery.classList.toggle('hidden')
+}
--- js/meme.controller.js
+++ js/meme.controller.js
@@ -53,6 +53,7 @@ function renderMeme(selectedImgId) {
         })
 
         updateDownloadLink()
+        hideGallery()
     }
 }
 
