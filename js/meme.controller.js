3a90fcacca19f2e7e72c5b620ffbd430ae2fc4d1
DvirFramework <78683107+DvirFramework@users.noreply.github.com>
2023-11-30 13:26:21 +0200

    add delete button

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -2,23 +2,19 @@
 const gCanvas = document.getElementById('canvas')
 const gCtx = gCanvas.getContext('2d')
 const textInput = document.getElementById('textInput')
-
 const colorPicker = document.getElementById('colorPicker')
 const increaseFontSizeBtn = document.getElementById('increaseFontSize')
 const decreaseFontSizeBtn = document.getElementById('decreaseFontSize')
-
 const addLineBtn = document.getElementById('addLineBtn')
-addLineBtn.addEventListener('click', addNewLine)
-
 const switchLineBtn = document.getElementById('switchLineBtn')
-switchLineBtn.addEventListener('click', switchLine)
+const deleteLineBtn = document.getElementById('deleteLineBtn')
 
+addLineBtn.addEventListener('click', addNewLine)
+switchLineBtn.addEventListener('click', switchLine)
 colorPicker.addEventListener('input', handleColorChange)
 increaseFontSizeBtn.addEventListener('click', increaseFontSize)
 decreaseFontSizeBtn.addEventListener('click', decreaseFontSize)
-
-gCanvas.addEventListener('click', onTextClick)
-
+deleteLineBtn.addEventListener('click', deleteLine)
 
 const selectedImgId = 2
 
@@ -30,12 +26,10 @@ function renderMeme(selectedImgId) {
     const selectedImg = gImgs.find(img => img.id === selectedImgId)
     const img = new Image()
 
-
     img.src = selectedImg.url
     img.onload = function () {
         gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-
 
         gMeme.lines.forEach((memeLine, idx) => {
             gCtx.font = `${memeLine.size}px Impact`
@@ -44,11 +38,12 @@ function renderMeme(selectedImgId) {
             gCtx.lineWidth = 2
 
             const textX = gCanvas.width / 2 - gCtx.measureText(memeLine.txt).width / 2
-            const textY = 50 + idx * 30
+            const textY = 30 + idx * 30 
 
             if (idx === gMeme.selectedLineIdx) {
                 gCtx.beginPath()
-                gCtx.rect(textX - 5, textY - 5, gCtx.measureText(memeLine.txt).width + 10, -memeLine.size - 10)
+                const textWidth = gCtx.measureText(memeLine.txt).width
+                gCtx.rect(textX - 5, textY - 5, textWidth + 10, -memeLine.size - 10)
                 gCtx.strokeStyle = 'black'
                 gCtx.stroke()
             }
@@ -62,8 +57,8 @@ function renderMeme(selectedImgId) {
 }
 
 function updateDownloadLink() {
-    const downloadLink = document.getElementById('downloadLink');
-    downloadLink.href = gCanvas.toDataURL();
+    const downloadLink = document.getElementById('downloadLink')
+    downloadLink.href = gCanvas.toDataURL()
 }
 
 textInput.addEventListener('input', function () {
@@ -101,6 +96,11 @@ function addNewLine() {
     renderMeme(gMeme.selectedImgId)
 }
 
+function deleteLine() {
+    gMeme.lines.splice(gMeme.selectedLineIdx, 1)
+    renderMeme(gMeme.selectedImgId)
+  }
+
 function switchLine() {
     gMeme.selectedLineIdx = (gMeme.selectedLineIdx + 1) % gMeme.lines.length
     renderMeme(gMeme.selectedImgId)
@@ -109,3 +109,5 @@ function switchLine() {
 function shareToFacebook() {
     alert('Sharing to Facebook...')
 }
+
+
